025de3f6ade8f4c347dbf43604347c57c74a7d6e
Gabe Piano <43084571+Poije@users.noreply.github.com>
2024-01-23 10:34:53 +0800

    task1 userInput

--- PrimeChecker.java
+++ PrimeChecker.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class PrimeChecker {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the upper bound: ");
+        int upperBound = scanner.nextInt();
+
+        System.out.print("Enter the number of threads: ");
+        int numThreads = scanner.nextInt();
+
+        // Rest of the code...
+
+        scanner.close();
     }
 }
