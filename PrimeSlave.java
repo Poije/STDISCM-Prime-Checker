e1955ac9c73b765010d0dc886eb0952e9392281d
Gabe Piano <43084571+Poije@users.noreply.github.com>
2024-02-23 21:15:58 +0800

    Update PrimeSlave.java

--- PrimeSlave.java
+++ PrimeSlave.java
@@ -1,18 +1,23 @@
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class PrimeSlave {
-    public static void main(String[] args) {
-        System.out.print ("Wating for Master");
+    public static void main(String[] args) throws IOException {
         Socket socket = new Socket("localhost", 12345); // Connect to server on localhost, port 12345
-        
+        System.out.println("Slave Connected to Server...");
+
         ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
         ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-
-
-
-        out.writeObject(new int[]{start, end});
+        try {
+            List<Integer> primes = new ArrayList<>();
+            int[] range = (int[]) in.readObject(); // Receive primes from server
+            primes = PrimeChecker.get_primes(range[0], range[1]);
+            out.writeObject(primes);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            socket.close();
+        }
     }
 }
