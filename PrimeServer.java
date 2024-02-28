ef3e13868037bf632ba0716557088129eea0268d
Alybee75 <alyana_erin_bondoc@dlsu.edu.ph>
2024-02-29 02:30:44 +0800

    optimize
    compressed data before sending to make it faster
    changed calculation of time based on specs
    removed comments

--- PrimeServer.java
+++ PrimeServer.java
@@ -1,14 +1,19 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.zip.GZIPOutputStream;
 
 public class PrimeServer {
     private static final int MASTER_SERVER_PORT = 12345;
-    //private static final int SLAVE_SERVER_PORT_1 = 12346;
-    //private static final int SLAVE_SERVER_PORT_2 = 12347;
-    private static final List<Integer> slavePorts = new ArrayList<>(); 
+    private static final List<Integer> slavePorts = Collections.synchronizedList(new ArrayList<>());
+    private static ExecutorService clientExecutor;
+    private static ExecutorService slaveExecutor;
 
     public static void main(String[] args) {
+        clientExecutor = Executors.newCachedThreadPool(); 
+        slaveExecutor = Executors.newFixedThreadPool(2); 
+
         startSlaveServer("Slave Server 1");
         startSlaveServer("Slave Server 2");
 
@@ -16,34 +21,34 @@ public class PrimeServer {
             System.out.println("Master Server started on port " + MASTER_SERVER_PORT);
 
             while (true) {
-                Socket clientSocket = serverSocket.accept(); // Accept incoming client connections
-                Thread clientThread = new Thread(new ClientHandler(clientSocket));
-                clientThread.start();
+                Socket clientSocket = serverSocket.accept();
+                clientExecutor.submit(new ClientHandler(clientSocket));
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            clientExecutor.shutdown();
+            slaveExecutor.shutdown();
         }
     }
 
-    // Probably have to fix this method to accomodate the seperate slave servers
     private static void startSlaveServer(String serverName) {
-        new Thread(() -> {
-            try (ServerSocket serverSocket = new ServerSocket(0)) { // Dynamically allocate a port
+        slaveExecutor.submit(() -> {
+            try (ServerSocket serverSocket = new ServerSocket(0)) {
                 int port = serverSocket.getLocalPort();
+                synchronized (slavePorts) {
+                    slavePorts.add(port);
+                }
                 System.out.println(serverName + " started on port " + port);
-        
-                // Register the slave server's port with the master server
-                slavePorts.add(port); 
-        
+
                 while (true) {
                     Socket clientSocket = serverSocket.accept();
-                    Thread clientThread = new Thread(new SlaveHandler(clientSocket));
-                    clientThread.start();
+                    new Thread(new SlaveHandler(clientSocket)).start();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }).start();
+        });
     }
 
     private static class ClientHandler implements Runnable {
@@ -57,15 +62,19 @@ public class PrimeServer {
         public void run() {
             try (ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
                  ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream())) {
-                    long startTime = System.nanoTime();
+
                     Object[] receivedData = (Object[]) in.readObject();
-                    int[] range = (int[]) receivedData[0]; // Extract range
-                    int numThreads = (Integer) receivedData[1]; // Extract number of threads
+                    long startTime = System.nanoTime(); 
+
+                    int[] range = (int[]) receivedData[0]; 
+                    int numThreads = (Integer) receivedData[1]; 
         
                     List<Integer> primes = distributeTask(range, numThreads);
                     long endTime = System.nanoTime();
                     long elapsedTimeMillis = (endTime - startTime) / 1000000;
-                    out.writeObject(new Object[]{primes, elapsedTimeMillis});
+                    System.out.println("Elapsed Time: " + elapsedTimeMillis); //check
+                    
+                    sendCompressedData(new Object[]{primes, startTime}, clientSocket);
             } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
@@ -73,15 +82,13 @@ public class PrimeServer {
 
         private List<Integer> distributeTask(int[] range, int numThreads) {
             int totalRange = range[1] - range[0] + 1;
-            int totalServers = slavePorts.size() + 1; // +1 for the master server
+            int totalServers = slavePorts.size() + 1; 
             List<Integer> allPrimes = new ArrayList<>();
         
-            // Calculate the size of each subrange considering the master server
             int subrangeSize = totalRange / totalServers;
             int remainder = totalRange % totalServers;
             int currentStart = range[0];
         
-            // Master server processes its subrange
             int masterEnd = currentStart + subrangeSize - 1;
             if (remainder > 0) {
                 masterEnd += 1;
@@ -89,12 +96,10 @@ public class PrimeServer {
             }
             System.out.println("Master Server at port: "  + clientSocket.getLocalPort() + " received range: [" + currentStart + ", " + masterEnd + "]");
             allPrimes.addAll(PrimeChecker.get_primes(currentStart, masterEnd, numThreads));
-            System.out.println("Master calculated primes: " + allPrimes);
+            // System.out.println("Master calculated primes: " + allPrimes);
         
-            // Update the start for the slave servers
             currentStart = masterEnd + 1;
         
-            // Distribute the rest of the range to the slave servers
             for (int slavePort : slavePorts) {
                 int slaveEnd = currentStart + subrangeSize - 1;
                 if (remainder > 0) {
@@ -103,34 +108,11 @@ public class PrimeServer {
                 }
                 int[] slaveSubrange = new int[]{currentStart, slaveEnd};
                 allPrimes.addAll(distributeSubRangeToSlave(slaveSubrange, slavePort, numThreads));
-                currentStart = slaveEnd + 1; // Update the start for the next slave server
+                currentStart = slaveEnd + 1; 
             }
         
             return allPrimes;
         }
-        
-
-        // use PrimeChecker.split_range(limit, numthreads) instead to split the range into subranges
-    /* 
-        private List<int[]> splitRange(int limit, int numParts) {
-            List<int[]> partitionedList = new ArrayList<>();
-            int range = limit / numParts;
-            int start = 0;
-            int end = range;
-
-            for (int i = 0; i < numParts; i++) {
-                int[] temp = {start, end};
-                partitionedList.add(temp);
-                start = end + 1;
-                end += range;
-            }
-
-            // Adjust the end of the last subrange to ensure it covers the entire range
-            partitionedList.get(numParts - 1)[1] = limit;
-
-            return partitionedList;
-        }
-    */
 
     // Probably have to fix this method to accomodate the seperate slave servers
         @SuppressWarnings("unchecked")
@@ -139,11 +121,28 @@ public class PrimeServer {
                  ObjectOutputStream out = new ObjectOutputStream(slaveSocket.getOutputStream());
                  ObjectInputStream in = new ObjectInputStream(slaveSocket.getInputStream())) {
         
-                out.writeObject(new Object[] {subrange, numThreads}); // Send subrange to slave server
-                return (List<Integer>) in.readObject(); // Receive primes from slave server
+                out.writeObject(new Object[] {subrange, numThreads}); 
+                return (List<Integer>) in.readObject(); 
             } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
-                return new ArrayList<>(); // Handle error gracefully
+                return new ArrayList<>(); 
+            }
+        }
+
+        private void sendCompressedData(Object data, Socket socket) {
+            try {
+                ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+                try (GZIPOutputStream gzipOut = new GZIPOutputStream(byteStream);
+                    ObjectOutputStream objectOut = new ObjectOutputStream(gzipOut)) {
+                    objectOut.writeObject(data);
+                } 
+
+                byte[] compressedData = byteStream.toByteArray();
+                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+                dos.writeInt(compressedData.length);
+                dos.write(compressedData);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
@@ -161,23 +160,18 @@ public class PrimeServer {
             try (ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
                  ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream())) {
     
-                // Receive range from master server
                 Object[] receivedData = (Object[]) in.readObject();
-                int[] range = (int[]) receivedData[0]; // Extract range
-                int numThreads = (Integer) receivedData[1]; // Extract number of threads
+                int[] range = (int[]) receivedData[0]; 
+                int numThreads = (Integer) receivedData[1]; 
                 System.out.println("Slave Server at port: " + clientSocket.getLocalPort() + " received range: " + Arrays.toString(range));
     
-                // Generate primes within the received range
                 List<Integer> primes = PrimeChecker.get_primes(range[0], range[1], numThreads);
-                System.out.println("Slave calculated primes: " + primes);
+                // System.out.println("Slave calculated primes: " + primes);
 
-    
-                // Send primes back to master server
                 out.writeObject(primes); 
             } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
         }
     }
-    
 }
